e2363717e1ffc691b148d4bca647bc06e31ac460
lsdengo <dvuchkov@hotmai.com>
2025-04-11 17:10:03 +0300

    Added trivia and about pages

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.run(debug=True)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+    return render_template("index.html")
+
+
+@app.route("/trivia")
+def trivia():
+    return render_template("trivia.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
