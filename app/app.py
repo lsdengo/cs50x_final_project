312a7d41f359fb72ef3062b9c51d596bb4e37ee7
lsdengo <dvuchkov@hotmail.com>
2025-04-23 14:13:42 +0300

    Completed DB and Search

--- app/app.py
+++ app/app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, request
+from cs50 import SQL
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+db = SQL("sqlite:///cars.db")
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 
 @app.route("/")
@@ -11,6 +22,31 @@ def index():
 @app.route("/trivia")
 def trivia():
     return render_template("trivia.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+@app.route("/search_car")
+def search_car():
+    query = request.args.get("q")
+    if query:
+        cars = db.execute(
+            "SELECT * FROM cars WHERE manufacturer LIKE ? COLLATE NOCASE", "%" + query + "%"
+        )
+    else:
+        cars = []
+    return jsonify(cars)
+
+
+@app.route("/car/<int:car_id>")
+def car_detail(car_id):
+    car = db.execute("SELECT * FROM cars WHERE id = ?", car_id)
+    if not car:
+        return "Car not found", 404
+    return render_template("search_car.html", car=car[0])
 
 
 @app.route("/about")
